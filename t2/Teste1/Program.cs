862a5754f56911616c911da53398c8197b975c77
diogo1987 <70672329+diogo1987@users.noreply.github.com>
2021-04-24 15:48:07 -0300

    Teste Prático BTP - Diogo Vieira Novais
    Teste Prático BTP - Diogo Vieira Novais - Respostas Teste Prático

---
+++ t2/Teste1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Teste1
+{
+    class Program
+    {
+        //TODO
+        /* 
+         * Desenvolva um método que receba a string "abbcbbb". O retorno desse método deverá ser "abcb", 
+         * logo o próximo caracter não deverá ser igual ao anterior, o método deve ser capaz de tratar qualquer string.
+        */
+        static void Main(string[] args)
+        {
+            String avaliacao = "abbcbbb";
+
+           string resultadoFinal = NaoRetornaCaracteresIgualAnterior(avaliacao);
+        }
+
+        //Verifica se a letra atual é igual a letra anterior dentro de uma string
+        private static string NaoRetornaCaracteresIgualAnterior(string textoAvaliacao)
+        {
+            string resultadoAvaliacao = string.Empty;
+            char letra;
+            
+            char letraAnterior = textoAvaliacao[0];
+            //Inicializa o resultadoAvaliacao com a primeira letra do textoAvaliacao
+            resultadoAvaliacao = resultadoAvaliacao.Insert(0, letraAnterior.ToString());
+            int contCaracteres = 0;
+            try
+            {
+                //Percorre todo o texto a começar pela segunda letra
+                for (int cont = 1; cont <= textoAvaliacao.Length-1; cont++ )
+                {
+                    letra = textoAvaliacao[cont];
+                    //Verifica se a letra atual é diferente a anterior
+                    if (letraAnterior != letra)
+                    {
+                        contCaracteres = contCaracteres + 1;
+                        //Insere no resultadoAvaliacao a letraAtual
+                        resultadoAvaliacao = resultadoAvaliacao.Insert(contCaracteres, letra.ToString());
+                    }
+
+                    //Atribui a letra anterior o valor da letra atual, para a proxima rodada
+                    letraAnterior = letra;
+                }
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+
+            //Retorna o resultado Avaliacao.
+            return resultadoAvaliacao;
+        }
+
+    }
+}
---
+++ t2/Teste2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Teste2
+{
+    /*
+      * TODO
+      * Implementar CanBeConnect para identificar se o Friend A pode se Conectar com o Friend C. Para isso, é necessário ter um friend em comum.
+         - Caso sim, o método deverá retornar True
+         - Caso não, o método deverá retornar False
+      */
+    class Friend
+    {
+        public string Email { get; private set; }
+
+        public ICollection<Friend> Friends { get; private set; }
+
+        public Friend(string email)
+        {
+            this.Email = email;
+            this.Friends = new List<Friend>();
+        }
+
+        public void AddFriendship(Friend friend)
+        {
+            this.Friends.Add(friend);
+        }
+
+        public bool CanBeConnect(Friend friendC)
+        {
+            //Lista aos amigos existes.
+            var amigos = this.Friends;
+
+            // Se o count for maior que 0, possui amigos
+            if (amigos.Count > 0)
+            {
+                // Varre na lista de amigos, para ver se algum tem friendC como amigo, caso sim, ele pode se conectar ao friendC.
+                foreach (var friend in amigos)
+                {
+                    if (friend.Friends.Contains(friendC))
+                    {
+                        return true;
+                        
+                    }
+                }
+            }         
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Friend a = new Friend("A");
+            Friend b = new Friend("B");
+            Friend c = new Friend("C");
+
+            a.AddFriendship(b);
+            b.AddFriendship(c);
+
+            //Pode se conectar ao friend c
+            var podeConectar = a.CanBeConnect(c);
+
+        }
+    }
+}
